4799a2264c55f312a8890d01823930708b332806
Jordan Geltch-Robb <jgeltchrobb@hotmail.com>
2018-03-18 01:10:26 +1000

    Main functionality completed

--- scripts.js
+++ scripts.js
@@ -25,24 +25,11 @@ function about_flip() {
 
 };
 
-// function scrollTo(to, duration) {
-//     if (document.body.scrollTop == to) return;
-//     var diff = to - document.body.scrollTop;
-//     var scrollStep = Math.PI / (duration / 10);
-//     var count = 0, currPos;
-//     start = element.scrollTop;
-//     scrollInterval = setInterval(function(){
-//         if (document.body.scrollTop != to) {
-//             count = count + 1;
-//             currPos = start + diff * (0.5 - 0.5 * Math.cos(count * scrollStep));
-//             document.body.scrollTop = currPos;
-//         }
-//         else { clearInterval(scrollInterval); }
-//     },10);
-// }
-
-// function test(elID)
-// {
-//     var dest = document.getElementById(elID);
-//     scrollTo(dest.offsetTop, 500);
-// }
+function port_overlay() {
+    document.getElementById('top_page').classList.toggle('background-content1');
+    document.getElementById('top_portfolio').classList.toggle('overlay1');
+    document.getElementById('overlay-content').classList.toggle('overlay-content1');
+    document.getElementById('my_name').classList.toggle('words_blur');
+    document.getElementById('full_stack').classList.toggle('words_blur');   
+    
+}
